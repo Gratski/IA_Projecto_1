3adf25767a56e647682929991553a5292cf59a34
Joao Rodrigues <JoaoRodrigues@Joaos-MacBook-Air-2.local>
2015-11-22 17:06:52 +0000

    versao final

--- new_maps/teampac500.pl
+++ new_maps/teampac500.pl
@@ -2,10 +2,152 @@
 % LAB 1 pacman500(1,300,0,(2,-9,1,90,0),(0,-9,1,90,0),[(3,8,1,270,0),(2,7,1,270,0)],(-9,1),(8,1),[(-3,3),(-9,-1),(-7,-7),(-1,9),(0,-7),(-8,1),(-7,-9),(-2,-5),(-5,8),(7,6),(-8,5),(2,6),(6,-3),(3,-5),(-1,-1),(7,-7),(-4,-5),(-8,-9),(-9,1),(-9,3),(8,3),(-7,-3),(4,6),(-5,6),(-1,3),(-1,-3),(-8,8),(-5,0),(4,-2),(-1,-4),(-8,-8),(-5,2),(1,3),(-7,5),(-5,-7),(-5,-4),(4,0),(7,3),(0,1),(-8,-4),(-3,2),(-6,7),(2,-7),(-7,-5),(3,1),(6,-7),(7,-1),(-2,3),(0,-5),(4,-4),(-5,-3),(1,-5),(4,3),(2,3),(-1,1),(6,-1),(-6,-3),(0,-4),(-1,5),(4,9),(0,7),(2,7),(-4,1),(4,-1),(-2,9),(-7,3),(-7,-1),(-8,9),(2,-3),(5,7),(7,8),(7,1),(2,1),(-6,5),(6,-5),(-1,-8),(-8,3),(-3,0),(2,-5),(3,-3),(-4,-9),(0,-1),(-7,-6),(-2,-3),(6,7),(-7,7),(2,-6),(0,3),(-5,1),(0,8),(2,9),(-3,-5),(-6,1),(1,5),(-7,9),(-5,-5),(7,9),(0,5),(3,7),(4,8),(4,2),(6,-6),(0,4),(8,-1),(0,-8),(-5,7),(-3,6),(-8,-7),(5,9),(6,-9),(-1,8),(7,5),(7,-5),(5,-7),(4,5),(7,7),(-5,-9),(1,9),(-6,9),(5,5),(-3,-2),(0,-9),(2,0),(-3,7),(-3,-6),(-2,-1),(-5,4),(-8,-1),(-5,5),(-1,7),(1,-9),(-2,5),(1,-3),(-2,7),(4,7),(7,-8),(1,-7),(-8,7),(-3,-3),(5,-3),(2,-1),(-3,-7),(-5,-2),(-2,-9),(7,-3),(6,3),(0,-3),(-1,4),(-5,-6),(-7,1),(7,-9),(-3,1),(-8,6),(-5,9),(-3,-1),(-1,-7),(3,9),(-5,-1),(-8,-5),(2,2),(-3,-9),(6,5),(3,-9),(1,-1),(-4,-3),(5,-9),(-8,-3),(-6,-9),(4,-3),(6,1),(8,1),(5,1),(7,-4),(-4,9),(-3,5),(1,7),(6,9),(-6,-7),(4,-5),(4,4),(0,9),(4,-7),(2,-9),(-1,-9),(4,-9),(2,5),(4,-6),(-3,9),(2,-2),(-4,7),(-1,-5),(4,1),(-2,-7),(-5,3)],[(-6,-7),(-7,5),(-5,2),(-3,-9),(-7,-6),(-2,-5),(-8,-3),(-7,-7),(-4,9),(-8,9),(-6,-9),(-5,-9),(-8,-9),(-7,-3),(-5,7),(-4,7),(-6,5),(-7,9),(-3,7),(-6,7),(-8,6),(-5,-1),(-5,9),(-8,-4),(-5,6),(-4,-9),(-8,-7),(-2,-7),(-5,-2),(-8,-8),(-2,9),(-5,4),(-3,9),(-4,-3),(-3,6),(-2,-3),(-8,7),(-4,-5),(-7,-9),(-3,-3),(-7,7),(-3,5),(-5,-5),(-8,5),(-1,-7),(-5,-6),(-3,-7),(-6,9),(-1,-5),(-2,7),(-1,5),(-1,-8),(-1,-3),(-5,5),(-2,5),(-2,-9),(-1,8),(-5,-7),(-1,-9),(-1,7),(-3,-5),(-1,9),(-7,-5),(-3,-6),(-5,8),(-5,1),(-5,-3),(-6,-3),(-5,0),(-5,-4),(-5,3),(-1,-4)],[(-8,-5),(-8,8)],[(4,-2),(6,-3),(6,5),(7,-9),(4,-7),(7,5),(5,7),(7,9),(2,-5),(4,3),(6,-6),(0,-4),(0,-5),(1,7),(4,7),(5,-3),(0,9),(0,5),(0,8),(4,8),(4,2),(4,4),(6,-7),(3,-3),(5,-9),(4,-3),(1,-5),(7,7),(4,9),(2,-7),(0,-9),(0,-3),(4,-1),(4,6),(2,-9),(6,-5),(7,6),(6,-9),(3,9),(3,-9),(1,5),(2,6),(1,-3),(5,9),(2,7),(0,-8),(4,-9),(7,-4),(4,-4),(1,-9),(5,5),(7,-7),(4,1),(2,-3),(7,-3),(4,0),(1,9),(7,-8),(4,-6),(2,5),(1,-7),(0,-7),(2,9),(3,7),(6,9),(2,-6),(0,7),(4,-5),(3,-5),(4,5),(6,7),(5,-7)],[(7,-5),(7,8)],Decisao).
 
 
+%%-- EM ALERTA %	 
+pacman500(_,_,_,(Id,PacX,PacY,PacDir,_),_,Enemies,_,_,Free,_,_,Pastilhas,MaxPastilhas,Dec) :-
+	write('primeira Func '),
+	alert_11((Id,PacX,PacY,PacDir,_), Enemies, 2,ListWithEnemy),
+	ListWithEnemy \== [],
+	write('Alerta ativado '),write(ListWithEnemy), nl,
+	first(ListWithEnemy, Ene),
+	sucs_11_2((PacX,PacY), 0, Free, Sucs),
+	setof((Valor,X,Y), X^Y^(member((_,X,Y), Sucs), casa_Value_11( Id,(X,Y),Ene, Pastilhas, MaxPastilhas, Valor)), Lista ),
+	write('Lista (valor Suc)'), write(Lista), nl,
+	reverse(Lista, ListaR),
+	first(ListaR, (_,Xx,Yy) ),
+	write('Viz:'), write(PacX), write(PacY), write(','), write(Xx), write(Yy), nl,
+	viz_11(Dec,(PacX,PacY), (Xx,Yy) ).
 
-%%-- EM ALERTA %
-pacman500(_,_,_,(Id,PacX,PacY,PacDir,_),_,Enemies,_,_,Free,_,_,Pastilhas,MaxPastilhas,Dec):-
-	alerta( (PacX, PacY), Enemies, Dec).
+%%-- %
+%para par
+pacman500(N,_,_,(Id,PacX,PacY,PacDir,_),_,[(IdAdv,_,_,Dir,_),_],_,_,Free,_,_,Pastilhas,MaxPastilhas,Decisao) :-
+	N < 10,
+	MyIdMod is mod(Id, 2), MyIdMod == 0, AdvIdMod is mod(IdAdv, 2), AdvIdMod == 0,
+	simetrico(Dir, Decisao).
+pacman500(N,_,_,(Id,PacX,PacY,PacDir,_),_,[_,(IdAdv,_,_,Dir,_)],_,_,Free,_,_,Pastilhas,MaxPastilhas,Decisao) :-
+	N < 10,
+	MyIdMod is mod(Id, 2), MyIdMod == 0, AdvIdMod is mod(IdAdv, 2), AdvIdMod == 0,
+	simetrico(Dir, Decisao).
+
+%para impar
+pacman500(N,_,_,(Id,PacX,PacY,PacDir,_),_,[(IdAdv,_,_,Dir,_),_],_,_,Free,_,_,Pastilhas,MaxPastilhas,Decisao) :-
+	N < 10,
+	MyIdMod is mod(Id, 2), MyIdMod == 1, AdvIdMod is mod(IdAdv, 2), AdvIdMod == 1,
+	simetrico(Dir, Decisao).
+pacman500(N,_,_,(Id,PacX,PacY,PacDir,_),_,[_,(IdAdv,_,_,Dir,_)],_,_,Free,_,_,Pastilhas,MaxPastilhas,Decisao) :-
+	N < 10,
+	MyIdMod is mod(Id, 2), MyIdMod == 1, AdvIdMod is mod(IdAdv, 2), AdvIdMod == 1,
+	simetrico(Dir, Decisao).
+
+simetrico(0,0).
+simetrico(180,180).
+simetrico(90,270).
+simetrico(270,90).
+
+
+%%-- ALERTA %
+alert_11(MyPos, EnemyPosList, AlertDist, ListWithEnemy):-
+	write('alert1'), nl,
+	insideRadius_11(MyPos,EnemyPosList, AlertDist,EnemyListClose),
+	write('alert2 '),write(EnemyListClose), nl,
+	haveLowFear_11(EnemyListClose,AlertDist, ListWithEnemy),!.
+
+%'verifica se estamos no campo do inimigo. Return True ou False.'
+enemyField_11((Id,PosX,_)):-
+	Id < 2,
+	PosX > (-1).
+
+enemyField_11((Id,PosX,_)):-
+	Id > 2,
+	PosX < 0.
+
+%'verifica se algum inimigo esta a uma distancia inferior a alertDist.'
+insideRadius_11(_,[], _, []).
+insideRadius_11((_,MyX,MyY,_,_),List, AlertDist, EnemyListClose):-
+	first(List, (_,PosX,PosY,_,_)),
+	X is MyX - PosX,
+	Y is MyY - PosY,
+	X2 is X^2,
+	Y2 is Y^2,
+	Sq is X2+ Y2,
+	Res is sqrt(Sq),
+	AlertDistX is AlertDist + 1,
+	Res =< AlertDistX,
+	tail(List,R),
+	insideRadius_11((_,MyX,MyY,_,_),R, AlertDist, EnemyListCloseTemp),
+	first(List,H),
+	EnemyListClose = [H|EnemyListCloseTemp].
+	
+insideRadius_11(Pos,List, AlertDist, EnemyListClose):-
+	tail(List,R),
+	insideRadius_11(Pos,R, AlertDist, EnemyListClose2),
+	append([],EnemyListClose2, EnemyListClose).
+
+%'verifica se os inimigos estão com medo inferior a alertDist.Retorna a lista de inimigos com medo inferior a alertDist ou vazio.'
+haveLowFear_11([],_,[]).
+haveLowFear_11(EnemyList,AlertDist, Res):-
+	first(EnemyList, (_,PosX,PosY,_,M)),
+	M  < AlertDist,
+	tail(EnemyList,T),
+	haveLowFear_11( T,AlertDist, ResTemp),
+	Res = [(PosX,PosY) | ResTemp].
+	
+haveLowFear_11(EnemyList,AlertDist,Res):-
+	tail(EnemyList,T),
+	haveLowFear_11(T,AlertDist,ResTemp),
+	append([],ResTemp,Res).
+
+avaliaPosicao_11(Id,(PosX,PosY), Enemy, Pastilhas, MaxPastilhas, Valor ):-
+	enemyField_11((Id,PosX,PosY)),
+	% 'ALterar Para Dist Real'
+	manhatan_11((PosX,PosY), Enemy, Dist),
+	write('distE= '),write(Dist),nl,
+	comPastilha_11((PosX,PosY), Pastilhas, V),
+	comMaxPastilha_11((PosX,PosY), MaxPastilhas, V2),
+	Raiz is Dist * Dist,	
+	Raiz1 is Raiz + 1,
+	Temp is -10/Raiz1,
+	write('Temp '), write(Temp), nl,
+	write('V '), write(V), nl,
+	write('V2 '), write(V2), nl,
+	Temp2 is Temp + V,
+	write('Temp2 '), write(Temp2), nl,
+	Valor is Temp2 + V2,
+	write('Valor: '), write(Valor), nl.
+	
+%'caso seja no nosso Terreno'
+avaliaPosicao_11(Id,(PosX,PosY), Enemy, Pastilhas, MaxPastilhas, Valor ):-
+	manhatan_11((PosX,PosY), Enemy, Dist),
+	write(Dist),nl,
+	Raiz is Dist * Dist,
+	Raiz2 is Raiz +1,
+	Valor is 10/Raiz2.
+	
+casa_Value_11(Id,(PosX,PosY), Enemy, Pastilhas, MaxPastilhas, V):-
+	avaliaPosicao_11(Id,(PosX,PosY), Enemy, Pastilhas, MaxPastilhas, Valor ),!,
+	V is Valor.
+	
+	
+%'se a casa tiver pastilha devolve valor 5'
+comPastilha_11(Pos, Pastilhas, V ):-
+		member(Pos, Pastilhas),
+		V is 5.
+comPastilha_11(_,_, 0).
+
+%'se a casa tiver pastilha devolve valor 5'
+comMaxPastilha_11(Pos, Pastilhas, V ):-
+		member(Pos, Pastilhas),
+		V is 10,
+		write('Pastilhas'), write(Pastilhas), nl, 
+		write('Membro'), nl.
+comMaxPastilha_11(_,_, 0).
+
+first([],[]).	
+first([ H|_], H).
+second([F,S|_], S).
+tail([],[]).
+tail([_|T], T).
+soma(X,Y, Sum):-
+	Sum is X + Y.
 
 %%-- A EXPLORAR %
 pacman500(_,_,_,(Id,PacX,PacY,PacDir,_),_,Enemies,_,_,Free,_,_,Pastilhas,MaxPastilhas,Dec):-
@@ -18,6 +160,9 @@ pacman500(_,_,_,(Id,PacX,PacY,PacDir,_),_,Enemies,_,_,Free,_,_,Pastilhas,MaxPast
 		setof( (D, (X, Y)), ( X^Y^member( (D, (X, Y)), Distancias ) ), [ (D1, (X1, Y1)) | RorderdDistancias] ),
 		aStar_11( (PacX,PacY), [[1, 0, (PacX, PacY)]], (X1, Y1), Free, [], [], Dec ).
 
+
+
+%%-- CALCULA DISTANCIA A PASTILHAS DE VARIOS PONTOS %
 
 %%-- FUNCAO ALERTA %
 alerta( _, [], [] ).
@@ -144,6 +289,9 @@ applyHeuristic_11( Custo, [ P| R ], Goal, Sol ):-
 %%-- SUCESSORES %
 sucs_11( Pos, Free, Visitados, R ):-
 	findall( (SucX, SucY), ( viz_11(_, Pos, (SucX, SucY)), \+member( (SucX, SucY), Visitados ), member( (SucX, SucY), Free )), R ).
+
+sucs_11_2((PosX,PosY), C, Free, Sucs):-
+	findall((Nc,(X,Y)), (viz_11(_,(PosX,PosY),(X,Y)),member((X,Y),Free),Nc is C + 1), Sucs).
 
 %%-- MANHANTAN %
 manhatan_11( (X1, Y1), (X2, Y2), Dist ):-
